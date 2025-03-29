1ac4d6e0289bd732b60529d2be22630df24102b7
omEAGRAWAL <agrawalom744@gmail.com>
2025-03-29 17:07:18 +0530

    connection to postgreSql added

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,23 @@
-package cmd
+package main
+
+import (
+	"fmt"
+	"gocloud/internals/models"
+	"gocloud/internals/services"
+)
+
+func main() {
+	conn := services.GetDb()
+
+	err := services.InsertUser(conn, models.User{Name: "om", Email: "agraw333a55l", Password: "om"})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	email, err := services.GetUserByEmail(conn, "agrawahhgygul")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(email)
+}
